2720efc7dfae546b5f61ec44b0a4e503ab8ea573
unknown <sirdesmond09@gmail.com>
2021-04-23 10:35:23 +0100

    added user signup and login

--- bmi/views.py
+++ bmi/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Bmi
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 from math import pi
 from bokeh.plotting import figure
@@ -85,3 +88,46 @@ def home(request):
             context["div"] = div
             
     return render(request, 'index.html', context)
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        first_name  = request.POST['firstname']
+        last_name   = request.POST['lastname']
+        email      = request.POST['email']
+        password1   = request.POST['password1']
+        password2   = request.POST['password2']
+        username   = request.POST['username']
+
+        if password1 == password2:
+            user  = User.objects.create_user(username= username, first_name=first_name, last_name=last_name, email= email, password= password1, is_active = True)
+
+            login(request, user)        
+            
+            return redirect('home')
+        else:
+            messages.warning(request, "Please enter matching passwords")
+            return redirect('signup')
+
+    return render(request, 'signup.html')
+
+
+def login_view(request):
+    if request.POST:
+        username      = request.POST['username']
+        password   = request.POST['password']
+        user       = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.warning(request, "Invalid username or password!")
+    return render(request, 'login.html')
+    
+
+def logout_view(request):
+    logout(request)
+    
+    return redirect('home')
